654b31ced40a47d7cb3bd0d678a73b4e24c9a953
Aldahunter <alex.d.hunt@icloud.com>
2021-12-14 22:52:23 +0000

    Continue Screenshot Mananger Tests
    Create second test 'testTakeAndSaveElementScreenshot'.

--- src/main/java/com/qa/examples/seleniumexample/utilities/ScreenshotManager.java
+++ src/main/java/com/qa/examples/seleniumexample/utilities/ScreenshotManager.java
@@ -3,9 +3,11 @@ package com.qa.examples.seleniumexample.utilities;
 import java.io.File;
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ScreenshotManager {
 	
@@ -15,14 +17,26 @@ public class ScreenshotManager {
 		currentScreenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 	}
 	
+	public void takeElementScreenshot(WebDriver driver, By selector ) {
+		WebElement element = driver.findElement(selector);
+		currentScreenshot = element.getScreenshotAs(OutputType.FILE);
+	}
+	
 	public void saveScreenshot(String path) throws IOException {
-		currentScreenshot.renameTo(new File(path));
+		File screenshotFile = new File(path);
+		screenshotFile.getParentFile().mkdirs();
+		currentScreenshot.renameTo(screenshotFile);
 		currentScreenshot.createNewFile();
 	}
 	
 	public void takeAndSaveScreenshot(WebDriver driver, String path) throws IOException {
 		takeScreenshot(driver);
 		saveScreenshot(path);
-	}	
+	}
+	
+	public void takeAndSaveElementScreenshot(WebDriver driver, By selector, String path) throws IOException {
+		takeElementScreenshot(driver, selector);
+		saveScreenshot(path);
+	}
 
 }
--- src/test/java/com/qa/examples/seleniumexample/tests/screenshotManager/TestScreenshotManager.java
+++ src/test/java/com/qa/examples/seleniumexample/tests/screenshotManager/TestScreenshotManager.java
@@ -9,7 +9,9 @@ import java.time.Duration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -46,28 +48,77 @@ public class TestScreenshotManager {
 		// Close web driver after every test to prevent memory leaks
 		driver.quit();
 	}
+	
+	// Helper Class for gathering screenshot saving assertion values
+	private class ScreenshotSaveAssertionValues {
+		public File screenshotFile;
+		public String assertErrMsg;
+		
+		private ScreenshotSaveAssertionValues(String screenshotPath) throws IOException {
+			this.screenshotFile = new File(screenshotPath);
+			this.assertErrMsg = String.format("The screenshot '%s' does not exists", this.screenshotFile.getCanonicalPath());
+		}
+	}
+	
 
 	@Test
 	public void testTakeAndSaveScreenshot() throws IOException {
-		// Input and expected values
+		// Input URLs and Paths
 		String url = "https://www.bbc.co.uk/";
 		String screenshotPath = targetScreenshotPath + "testTakeAndSaveScreenshot.png";
+		
+		// Input and Expected Queries
 		String expectedTitle = "BBC - Home";
 		
-		// Go to the URL
-		navigator.to(url);
 		
-		// Assert we have gone to the correct URL
+		// Go to URL and check we are there
+		navigator.to(url);
 		assertEquals(expectedTitle, driver.getTitle());
 		
-		// Take a screenShot
+		// Take screenshot and check there is an image where screenshot is expected
 		screenshotManager.takeAndSaveScreenshot(driver, screenshotPath);
+		ScreenshotSaveAssertionValues screenshotSaveAssertValues = new ScreenshotSaveAssertionValues(screenshotPath);
+		assertTrue(screenshotSaveAssertValues.assertErrMsg, screenshotSaveAssertValues.screenshotFile.exists());
+	}
+	
+	@Test
+	public void testTakeAndSaveElementScreenshot() throws IOException {
+		// Input URLs and Paths
+		String url = "https://www.google.co.uk/";
+		String cookiesScreenshotPath = targetScreenshotPath + "testTakeAndSaveElementScreenshot-noCookiesPopUp.png";
+		String inputQueryScreenshotPath = targetScreenshotPath + "testTakeAndSaveElementScreenshot-searchBarInputtedQuery.png";
+		
+		// Input and Expected Queries
+		String searchQuery = "Pigs";
+		String expectedQuery = "Pigs";
+		
+		// Web element selectors
+		By cookiesPopUpBtnSelector = By.xpath("//button/div[text()='I agree']/..");
+	    By searchBarInptSelector = By.name("q");
+		
+	    
+		// Go to URL and get required web elements
+		navigator.to(url);
+		WebElement cookiesPopupButton = driver.findElement(cookiesPopUpBtnSelector);
+		WebElement searchBar = driver.findElement(searchBarInptSelector);
+		
+		// Close and check cookies pop-up is closed
+		cookiesPopupButton.click();
+		assertTrue("The Cookies Pop-Up is still displayed", !cookiesPopupButton.isDisplayed());
+		screenshotManager.takeAndSaveScreenshot(driver, cookiesScreenshotPath);
 		
 		// Assert we have created a file where the screenshot should be
-		File screenshotFile = new File(screenshotPath);
-		String assertErrMsg = String.format("The screenshot '%s' does not exists",
-											screenshotFile.getCanonicalPath());
-		assertTrue(assertErrMsg, screenshotFile.exists());
+		ScreenshotSaveAssertionValues screenshotSaveAssertValues = new ScreenshotSaveAssertionValues(cookiesScreenshotPath);
+		assertTrue(screenshotSaveAssertValues.assertErrMsg, screenshotSaveAssertValues.screenshotFile.exists());
+		
+		// Type query into search bar and check it was inputed
+		searchBar.sendKeys(searchQuery);
+		assertEquals(expectedQuery, searchBar.getAttribute("value"));
+		screenshotManager.takeAndSaveElementScreenshot(driver, searchBarInptSelector, inputQueryScreenshotPath);
+		
+		// Assert we have created a file where the screenshot should be
+		screenshotSaveAssertValues = new ScreenshotSaveAssertionValues(inputQueryScreenshotPath);
+		assertTrue(screenshotSaveAssertValues.assertErrMsg, screenshotSaveAssertValues.screenshotFile.exists());
 	}
 
 }
