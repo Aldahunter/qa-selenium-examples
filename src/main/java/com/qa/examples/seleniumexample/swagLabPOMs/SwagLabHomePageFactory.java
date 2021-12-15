63d232eb1de0d76cc3dbeb866bbf8c32b6605152
Aldahunter <alex.d.hunt@icloud.com>
2021-12-15 00:30:32 +0000

    Continue SwagLab POM Tests
    Create 'SwagLabHomePageFactory' POM.

--- src/main/java/com/qa/examples/seleniumexample/swagLabPOMs/SwagLabHomePageFactory.java
+++ src/main/java/com/qa/examples/seleniumexample/swagLabPOMs/SwagLabHomePageFactory.java
@@ -1,5 +1,32 @@
 package com.qa.examples.seleniumexample.swagLabPOMs;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class SwagLabHomePageFactory {
+	private String URL = "https://www.saucedemo.com/inventory.html";
+	private String expectTitle = "Swag Labs";
+
+	private WebDriver driver;
+
+	private By inventoryItemDivSelector = By.className("inventory_item");
 	
+	
+	public SwagLabHomePageFactory(WebDriver driver) {
+		this.driver = driver;
+		
+		driver.get(URL);
+
+		if (!driver.getTitle().equals(expectTitle)) {
+			String exceptionMsg = String.format("The Swag Lab Home Page did not load (%f)", URL);
+			throw new IllegalStateException(exceptionMsg);
+		}
+	}
+	
+	public List<WebElement> getInventoryItems() {
+		return driver.findElements(inventoryItemDivSelector);
+	}
 }
